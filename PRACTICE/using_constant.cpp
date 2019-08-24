e12520c971b8ce9aa782ed4ec11cf9570bf23dd5
Sunil Raj <47179226+PVSSLR@users.noreply.github.com>
2019-08-24 23:14:12 +0530

    Create using_constant.cpp

---
+++ PRACTICE/using_constant.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+const float tax{0.06};
+const int day{30};
+int small_room{0},large_room{0};
+int main() {
+	cout<<"Welcome to Sunil carpet cleaning service\n\n";
+	cout<<"Enter the number of small rooms :";
+	cin>> small_room;
+	cout<<"Enter the number of large rooms: ";
+	cin>> large_room;
+	cout<<"Charges: ";
+	cout<<"/nTax is: $"<< tax*day*(small_room + large_room)<<endl;
+	cout<<"Total Estimate is: $"<< (small_room*25)*(large_room*35)+(tax*day*(small_room + large_room));
+	cout<<endl;
+	return 0;
+}
