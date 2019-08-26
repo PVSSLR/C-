0901050d75c49d10f491162345b89bc3fe332811
Sunil Raj <47179226+PVSSLR@users.noreply.github.com>
2019-08-27 01:10:04 +0530

    Create array_basic.cpp

---
+++ PRACTICE/array_basic.cpp
@@ -0,0 +1,9 @@
+//ARRAY
+
+#include <iostream>
+using namespace std;
+char vowels [5] {'a','e','i','o','u'};
+int main() {
+	cout<<"The first one is: "<< vowels[0]<<endl;
+	cout<<"The last one is: "<< vowels[4]<<endl;
+}
