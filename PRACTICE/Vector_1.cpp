60edffe48b5345b18fea306694d4c8676a155cd1
Sunil Raj <47179226+PVSSLR@users.noreply.github.com>
2019-08-27 11:15:38 +0530

    Create Vector_1.cpp
    CHARACTERISTICS OF VECTORS
    
    1.Dynamic size 
    2.Element are all same types
    3.Individual elements can be accessed by their position or index 
    4.First element is 0 
    5.Last element is-1 
    
    6.Many fuctions to do bound checks 
    7.All the elements are zero initially 
    8.Very efficient 
    9.Used in looping

---
+++ PRACTICE/Vector_1.cpp
@@ -0,0 +1,27 @@
+//We will be using vector in c++
+//vector is a dynamic array 
+//c++ vector is nothing but a c++ vector 
+// We can do bounds checking 
+// we can also use functions such as sort, reverse, find and etc 
+#include <iostream>
+#include <vector> //we should first include the vector library 
+
+using namespace std;
+
+vector <char> vowels; //empty vector with character
+vector <int> numbers; //empty vector with integer
+
+vector <char> vowel (5); 
+vector <int> numbers (10);
+
+vector <char> vow {'a','b'};
+vector <int> number {1,2,3,4,5,6,7};
+vector <double> temp (365,80);
+
+//Accessing and modifying vector elements
+//Vector_name [index]
+//Vector_name.at(index)
+//Vector_name.push_back(elements) //at value at end of the vector
+
+
+
