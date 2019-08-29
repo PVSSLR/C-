06b4378bb90dba370519cb26b770ae55b77ae68b
Sunil Raj <47179226+PVSSLR@users.noreply.github.com>
2019-08-29 23:48:02 +0530

    Create boolalpha.cpp

---
+++ boolalpha.cpp
@@ -0,0 +1,15 @@
+//simple inc0eament 
+
+#include<iostream>
+using namespace std;
+
+bool total {5==5};
+
+
+int main() {
+	cout<<boolalpha;
+	cout<<total<<endl;
+	bool total {5==5};
+	cout<<total<<endl;
+	
+}
