84d6d393bba5bb83885094f2f5825b783675795f
Sunil Raj <47179226+PVSSLR@users.noreply.github.com>
2019-09-04 00:00:56 +0530

    Update compound_Assignment.cpp

--- PRACTICE/compound_Assignment.cpp
+++ PRACTICE/compound_Assignment.cpp
@@ -1,1 +1,25 @@
+//Compound Assignments 
+// += 
+//-=
+//*=
+// /=
+// >>=
+//   <<=
+// &=
+//^=
+//|=
+
+
 #include<iostream>
+using namespace std;
+int a{10};
+int b{20};
+int main() {
+	a+=1;
+	cout<<a<<endl;
+	b*=5;
+	cout<<b<<endl;
+	return 0;
+}	
+     
+   
