fe079e7df1ffb422acc68ee0ee22beb690ac491b
Sunil Raj <47179226+PVSSLR@users.noreply.github.com>
2019-09-06 00:13:25 +0530

    Create if_else_Statement.cpp

---
+++ PRACTICE/Flow_Control/if_else_Statement.cpp
@@ -0,0 +1,15 @@
+ //if else statement 
+  
+#include<iostream>
+using namespace std;
+int altitude{11};
+
+int main() {
+	if (altitude<=11)
+		cout<<"Gradient Layer"<<endl;
+  else
+   cout<<"Maybe Isothermal layer or above 20km"<<endl;
+	return 0;
+}	
+     
+   
