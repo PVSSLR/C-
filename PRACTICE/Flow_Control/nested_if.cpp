b381f5e33130e00b126beb828d08b954ace6a584
Sunil Raj <47179226+PVSSLR@users.noreply.github.com>
2019-09-06 00:29:40 +0530

    Create nested_if.cpp

---
+++ PRACTICE/Flow_Control/nested_if.cpp
@@ -0,0 +1,23 @@
+ //nested if statement 
+   //if if else statement 
+  
+  //Compound Assignments 
+
+#include<iostream>
+using namespace std;
+int altitude{};
+int alt{};
+int main() {
+	cout<<"Enter altitude: "<<endl;
+	cin>>altitude;
+	if (altitude<=20)
+		cout<<"Enter altitude less than 20: "<<endl;
+	   cin>>alt;
+	  if(alt<11)
+		cout<<"Gradient Layer"<<endl;
+	   else
+	    cout<<" Isothermal layer"<<endl;
+	return 0;
+}	
+     
+   
