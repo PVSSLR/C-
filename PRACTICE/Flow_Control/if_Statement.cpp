db545e6bdd632655a10a0ce411cd6317ba293c06
Sunil Raj <47179226+PVSSLR@users.noreply.github.com>
2019-09-06 00:06:01 +0530

    Update if_Statement.cpp

--- PRACTICE/Flow_Control/if_Statement.cpp
+++ PRACTICE/Flow_Control/if_Statement.cpp
@@ -1,1 +1,20 @@
+//if statement 
+//if (expr)
+// statement;
+//statement must be intended 
+//if true the statement is executed 
+//if it is false it is skipped
 
+//Compound Assignments 
+
+#include<iostream>
+using namespace std;
+int a{10};
+int b{10};
+int main() {
+	if (a==b)
+		cout<<"Yes"<<endl;
+	return 0;
+}	
+     
+   
